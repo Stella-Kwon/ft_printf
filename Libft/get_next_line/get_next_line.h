81a20ed134f579aefe36eca396b9a86c7061862a
Sumin <kwontnals@gmail.com>
2024-01-10 16:36:01 +0200

    without submodules

---
+++ Libft/get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skwon2 <skwon2@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/23 14:04:18 by suminkwon         #+#    #+#             */
+/*   Updated: 2024/01/05 15:37:02 by skwon2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin(char *previous_read, char const *buffer);
+char	*ft_strchr(const char *s, int c);
+void	*free_one(char **buffer);
+#endif
